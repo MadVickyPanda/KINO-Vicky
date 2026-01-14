0b56542cbf529d44f2d6fd3d2d08832cfbc39b9c
MadVickyPanda <solboll@hotmail.com>
2026-01-14 17:11:32 +0100

    promises and loading index.html on localhost 5080

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 import express from 'express';
+import renderPage from './lib/renderPage.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = 5080;
 
-app.get('/', (req, res) => {
-  res.send('Express server fungerar!');
+app.get('/', async (req, res) => {
+    const html = await renderPage('index');
+    res.send(html);
 });
 
 app.listen(PORT, () => {
   console.log(`Server körs på http://localhost:${PORT}`);
 });
+
 
